bd399a600b6b29b2f62ae6f392ae9550d7e77982
Badsauce <Ninjazoby@gmail.com>
2014-12-02 02:41:01 -0500

    Cleaned up Puppet provisioning

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,23 +1,17 @@
-Exec{
-  path => ["/usr/bin", "/bin", "/usr/sbin", "/sbin", "/usr/local/bin", "/usr/local/sbin"]
- }
-
-include nodejs
-
-exec { "sym-link-node" :
-      cwd => "/vagrant",
-      command => "sudo ln -s /usr/bin/nodejs /usr/bin/node",
-      require => Package['nodejs'],
-  }
+class { 'nodejs':
+  version => 'stable',
+  make_install => false,
+}
 
 package { 'sails':
   ensure   => present,
   provider => 'npm',
-  require => Package['nodejs'],
+  require  => Class['nodejs']
 }
 
 exec { 'sails-depends' :
       cwd => '/vagrant',
-      command => 'sudo npm install',
+      command => 'npm install',
       require => Package['sails'],
+      path => ['/usr/local/node/node-default/bin/','/usr/bin/',"/usr/bin", "/bin", "/usr/sbin", "/sbin", "/usr/local/bin", "/usr/local/sbin"],
   }
